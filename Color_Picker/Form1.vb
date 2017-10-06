ec722310b8556209ef0e8993e1ff6b7676c1b17c
kikoreta <enr.gtz@outlook.com>
2017-10-06 04:01:58 -0500

    First Release

--- Color_Picker/Form1.vb
+++ Color_Picker/Form1.vb
@@ -1,3 +1,11 @@
-﻿Public Class Form1
+﻿'Simple Color Select Tool for getting RGB Values By Kikoreta_sop
 
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ColorDialog1.ShowDialog()
+        TextBox1.Text = ColorDialog1.Color.R
+        TextBox2.Text = ColorDialog1.Color.G
+        TextBox3.Text = ColorDialog1.Color.B
+        Panel2.BackColor = ColorDialog1.Color
+    End Sub
 End Class
