c4acb9c17053855e0d7de098ec7c9848034c9369
kikoreta <enr.gtz@outlook.com>
2017-10-24 03:45:43 -0500

    Major Update

---
+++ Color_Picker/Picker.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Picker
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Picker))
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(275, 17)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(36, 290)
+        Me.ListBox1.TabIndex = 2
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(12, 274)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(256, 30)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.InitialImage = Nothing
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(256, 256)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "Img_1.png")
+        Me.ImageList1.Images.SetKeyName(1, "Img_2.png")
+        Me.ImageList1.Images.SetKeyName(2, "Img_3.png")
+        Me.ImageList1.Images.SetKeyName(3, "Img_4.png")
+        Me.ImageList1.Images.SetKeyName(4, "Img_5.png")
+        '
+        'Picker
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ClientSize = New System.Drawing.Size(319, 316)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Picker"
+        Me.Text = "Picker"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ImageList1 As ImageList
+End Class
---
+++ Color_Picker/Picker.vb
@@ -0,0 +1,30 @@
+﻿Public Class Picker
+
+
+    Private Sub Picker_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        On Error Resume Next
+        Dim img As New Bitmap(1, 1)
+        img = PictureBox1.Image
+        Dim Px, Py
+        Px = (MousePosition.X - Left - PictureBox1.Left - 9)
+        Py = (MousePosition.Y - Top - PictureBox1.Top - 25)
+        PictureBox2.BackColor = img.GetPixel(Px, Py)
+    End Sub
+
+    Private Sub Picker_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Form1.TextBox1.Text = PictureBox2.BackColor.R
+        Form1.TextBox2.Text = PictureBox2.BackColor.G
+        Form1.TextBox3.Text = PictureBox2.BackColor.B
+    End Sub
+
+    Private Sub Picker_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For Each imagesNames As String In ImageList1.Images.Keys
+            ListBox1.Items.Add(imagesNames)
+        Next
+        ListBox1.SelectedIndex = 0
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        PictureBox1.Image = ImageList1.Images.Item(ListBox1.SelectedIndex)
+    End Sub
+End Class
